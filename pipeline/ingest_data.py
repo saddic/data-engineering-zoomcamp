9c0a8a4477e864d32073999b5bb26c24bac1e49f
Amoleka Ransford Saddic <65283034+saddic@users.noreply.github.com>
2026-01-23 21:20:27 +0000

    all

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -2,28 +2,31 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
+import click
+
+...
 
 
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
 
-
-
-def run_ingest():
+def run_ingest(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     # set up the database connection
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    # define the URL of the dataset
-    month = '01'
-    year = '2021'
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
+    # Format month with zero-padding
+    month_str = f'{month:02d}'
     prefix ='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month}.csv.gz'
+    url = f'{prefix}/yellow_tripdata_{year}-{month_str}.csv.gz'
 
     # define the data types for each column
     dtype = {
--- pipeline/pipeline.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pandas as pd
-
-print('Arguements passed to the script:', sys.argv)
-month = int(sys.argv[1])
-df = pd.DataFrame({'day':[1,2], 'number_of_passengers':[100, 150], 'month':[month, month]})
-print(df.head())
-df.to_parquet(f'output_{month}.parquet', index=False)
-print(f"Pipeline module loaded successfully. Month: {month}")
